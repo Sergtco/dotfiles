202b714eb7a64772090aed1d4f77773caf8aa7d8
sergtco <sergtcorge@gmail.com>
2024-10-08 22:17:58 +0300

    add some update

--- nvim/lua/keys/maps.lua
+++ nvim/lua/keys/maps.lua
@@ -9,8 +9,8 @@ vim.keymap.set("n", "<A-c>", "<cmd>tabclose<cr>", {})
 vim.keymap.set("n", "<leader>fe", require("oil").open_float, {})
 --Term
 vim.keymap.set("t", "<esc>", "<C-\\><C-N>", { silent = true })
-vim.keymap.set("n", "<c-/>", ToggleTerminal, { silent = true })
-vim.keymap.set("t", "<c-/>", ToggleTerminal, { silent = true })
+vim.keymap.set("n", "<c-\\>", ToggleTerminal, { silent = true })
+vim.keymap.set("t", "<c-\\>", ToggleTerminal, { silent = true })
 --global yank and paste
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>p", '"+p')
