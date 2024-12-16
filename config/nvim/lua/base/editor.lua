deecdef504bd992c91cc505e22bcc5fadf516a71
ivan <sergtcorge@gmail.com>
2024-12-16 22:11:09 +0300

    add russian langmap

--- config/nvim/lua/base/editor.lua
+++ config/nvim/lua/base/editor.lua
@@ -22,6 +22,9 @@ opt.softtabstop = indent
 opt.tabstop = indent
 opt.shiftround = true
 
+-- disable auto comment
+vim.cmd([[autocmd FileType * set formatoptions-=ro]])
+
 --ui
 opt.number = true
 opt.relativenumber = true
@@ -37,3 +40,21 @@ opt.guicursor = 'n-c:block,v:hor25,i-ci-ve:ver25,r-cr:hor20,o:hor50'
 
 --colors
 opt.termguicolors = true
+
+-- langmap
+local function escape(str)
+    -- You need to escape these characters to work correctly
+    local escape_chars = [[;,."|\]]
+    return vim.fn.escape(str, escape_chars)
+end
+
+local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
+local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
+local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
+local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
+
+vim.opt.langmap = vim.fn.join({
+    -- | `to` should be first     | `from` should be second
+    escape(ru_shift) .. ';' .. escape(en_shift),
+    escape(ru) .. ';' .. escape(en),
+}, ',')
