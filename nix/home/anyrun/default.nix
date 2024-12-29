59f14e7432c975e09dadd015bbae9852648c44dd
Ivan Nizelnik <iva.nizelnik@gmail.com>
2024-12-29 20:10:06 +0300

    Add zip

--- nix/home/anyrun/default.nix
+++ nix/home/anyrun/default.nix
@@ -156,13 +156,4 @@
         }
       '';
   };
-  nix.settings = {
-    extra-substituters = [
-      "https://anyrun.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
-    ];
-  };
 }
--- nix/home/terminals/ghostty.nix
+++ nix/home/terminals/ghostty.nix
@@ -14,13 +14,4 @@
   ];
 
   home.file.".config/ghostty/config".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/ghostty/config";
-  nix.settings = {
-    extra-substituters = [
-      "https://ghostty.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "ghostty.cachix.org-1:QB389yTa6gTyneehvqG58y0WnHjQOqgnA+wBnpWWxns="
-    ];
-  };
 }
