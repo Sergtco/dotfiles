7ab283b1f71c0eedbe76896c565806c0963fa78a
ivan <sergtcorge@gmail.com>
2025-01-22 13:14:38 +0300

    no nix-ld (for now)

--- nix/modules/programming/default.nix
+++ nix/modules/programming/default.nix
@@ -1,8 +1,5 @@
 {pkgs, ...}: {
   users.users.sergtco.extraGroups = ["docker"];
-  programs.nix-ld = {
-    enable = true;
-  };
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [
     #compilers 
