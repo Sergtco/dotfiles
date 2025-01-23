659b7d0e47435cd1fac2b5eb424df6aba07eb131
ivan <sergtcorge@gmail.com>
2025-01-23 18:17:19 +0300

    add some fixes

--- nix/hosts/sergt-pc/configuration.nix
+++ nix/hosts/sergt-pc/configuration.nix
@@ -100,6 +100,12 @@
       "monitor.bluez.properties" = {
         "bluez5.enable-sbc-xq" = true;
         "bluez5.enable-hw-volume" = true;
+        "bluez5.roles" = [
+          "a2dp_sink"
+          "a2dp_source"
+          "bap_sink"
+          "bap_source"
+        ];
       };
     };
   };
