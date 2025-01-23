37472c52565a136a3904859105a51f03e08b3102
ivan <sergtcorge@gmail.com>
2025-01-23 18:09:51 +0300

    format

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    config = function(_, opts)
-        require('nvim-treesitter.configs').setup(opts)
-    end,
-    opts = {
-        ensure_installed = { "vim", "vimdoc", "query", "comment", "markdown", "markdown_inline" },
-        sync_install = true,
-        highlight = {
-            enable = true,
-            disable = function(_, buf)
-                local max_filesize = 100 * 1024 -- 100 KB
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = { "ruby", "markdown" },
-        },
-        indent = { enable = true },
-    }
+	"nvim-treesitter/nvim-treesitter",
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
+	opts = {
+		ensure_installed = { "vim", "vimdoc", "query", "comment", "markdown", "markdown_inline" },
+		sync_install = true,
+		highlight = {
+			enable = true,
+			disable = function(_, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
+			additional_vim_regex_highlighting = { "ruby", "markdown" },
+		},
+		indent = { enable = true },
+	},
 }
