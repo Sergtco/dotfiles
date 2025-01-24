0903a5e00db9df212c1cc30359e3ef63d713bc71
ivan <sergtcorge@gmail.com>
2025-01-24 14:37:44 +0300

    some co

--- config/nvim/lua/base/editor.lua
+++ config/nvim/lua/base/editor.lua
@@ -36,35 +36,42 @@ opt.laststatus = 3
 opt.wrap = true
 opt.showbreak = "󱞶"
 
-opt.guicursor = 'n-c:block,v:hor25,i-ci-ve:ver25,r-cr:hor20,o:hor50'
+opt.guicursor = "n-c:block,v:hor25,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 
---colors
+-- colors
 opt.termguicolors = true
+
+-- create dir if not exists
+vim.cmd([[
+au BufWritePre,FileWritePre * if @% !~# '\(://\)' | call mkdir(expand('<afile>:p:h'), 'p') | endif
+]])
+
+
+-- highlights yanked text
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
+	end,
+})
 
 -- langmap
 local function escape(str)
-    -- You need to escape these characters to work correctly
-    local escape_chars = [[;,."|\]]
-    return vim.fn.escape(str, escape_chars)
+	-- You need to escape these characters to work correctly
+	local escape_chars = [[;,."|\]]
+	return vim.fn.escape(str, escape_chars)
 end
-
 local en = [[`qwertyuiop[]asdfghjkl;'zxcvbnm]]
 local ru = [[ёйцукенгшщзхъфывапролджэячсмить]]
 local en_shift = [[~QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>]]
 local ru_shift = [[ËЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]]
 
 vim.opt.langmap = vim.fn.join({
-    -- | `to` should be first     | `from` should be second
-    escape(ru_shift) .. ';' .. escape(en_shift),
-    escape(ru) .. ';' .. escape(en),
-}, ',')
-
--- highlights yanked text
-vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = "IncSearch",
-            timeout = 100,
-        })
-    end,
-})
+	-- | `to` should be first     | `from` should be second
+	escape(ru_shift)
+		.. ";"
+		.. escape(en_shift),
+	escape(ru) .. ";" .. escape(en),
+}, ",")
--- nix/modules/programming/default.nix
+++ nix/modules/programming/default.nix
@@ -2,7 +2,7 @@
   users.users.sergtco.extraGroups = ["docker"];
   virtualisation.docker.enable = true;
   environment.systemPackages = with pkgs; [
-    #compilers
+    #compilers 
     gcc
     clang
     unstable.python3
