cce4de633507745814cc3d057f8c492eaba0f86f
ivan <sergtcorge@gmail.com>
2025-10-12 21:49:47 +0300

    move whitelist
    move whitelist

--- nix/flake.nix
+++ nix/flake.nix
@@ -20,7 +20,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    stylix.url = "github:danth/stylix/release-25.05";
+    stylix = {
+      url = "github:danth/stylix/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
