62a9f6f79675a484ec2b1c99511a58a6c77a62fd
ivan <sergtcorge@gmail.com>
2025-11-09 22:47:47 +0300

    themes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -2,13 +2,14 @@ return {
 	{
 		"slugbyte/lackluster.nvim",
 		opts = {
-            tweak_ui = {
-                enable_end_of_buffer = true;
-            }
-        },
+			tweak_ui = {
+				enable_end_of_buffer = true,
+			},
+		},
 		priority = 500,
 		init = function()
 			vim.cmd.colorscheme("lackluster")
+			vim.api.nvim_set_hl(0, "Visual", { bg = "#242424", fg = "#aaaaaa" })
 		end,
 	},
 }
