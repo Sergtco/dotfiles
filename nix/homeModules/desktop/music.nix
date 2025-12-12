b5c4486ccf0185cbc16d2d0c677acf9717640980
ivan <sergtcorge@gmail.com>
2025-12-12 14:21:09 +0300

    add mpris

--- nix/homeModules/desktop/music.nix
+++ nix/homeModules/desktop/music.nix
@@ -1,4 +1,4 @@
-{...}: {
+{pkgs, ...}: {
   services.mpd = {
     enable = true;
     musicDirectory = "/home/sergtco/Music";
@@ -10,7 +10,23 @@
     '';
     network.startWhenNeeded = true;
   };
-  services.mpd-mpris.enable = true;
+
+  systemd.user.services.mpdris2-rs = {
+    Unit = {
+      Description = "Music Player Daemon D-Bus Bridge";
+      Wants = "mpd.service";
+      After = "mpd.service";
+    };
+    Service = {
+      Type = "dbus";
+      BusName = "org.mpris.MediaPlayer2.mpd";
+      Restart = "on-failure";
+      ExecStart = "${pkgs.mpdris2-rs}/bin/mpdris2-rs";
+    };
+    Install = {
+      WantedBy = ["default.target"];
+    };
+  };
 
   programs.rmpc = {
     enable = true;
