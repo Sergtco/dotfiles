837079e8e85149d457bf6ae5f71f2d8fcf1a1124
iva.nizelnik@gmail.com <sergtcorge@gmail.com>
2026-03-26 16:08:00 +0300

    fix git

--- modules/wrapped/git.nix
+++ modules/wrapped/git.nix
@@ -2,43 +2,45 @@
   self,
   inputs,
   ...
-}: {
-  perSystem = {
-    pkgs,
-    self',
-    ...
-  }: {
-    packages.git = inputs.wrapper-modules.wrappers.git.wrap {
-      inherit pkgs;
-      settings = {
-        user = {
-          name = "Ivan Nizelnik";
-          email = "iva.nizelnik@gmail.com";
-        };
-        alias = {
-          a = "add";
-          sw = "switch";
-          co = "checkout";
-          cc = "commit";
-          ca = "commit --amend";
-          p = "push";
-          f = "fetch";
-          st = "status";
-        };
-        core = {
-          editor = "nvim";
-          quotePath = false;
-        };
-        diff = {
-          tool = "nvimdiff";
-        };
-        merge = {
-          tool = "nvimdiff";
-        };
-        "includeIf \"gitdir:~/work/\"" = {
-          path = "~/work/.gitconfig";
-        };
+}:
+{
+  perSystem =
+    {
+      pkgs,
+      self',
+      ...
+    }:
+    {
+      packages.git = inputs.wrapper-modules.wrappers.git.wrap {
+        inherit pkgs;
+        configFile.content = /*git_config*/''
+          [user]
+          	email = "iva.nizelnik@gmail.com"
+          	name = "Ivan Nizelnik"
+
+          [alias]
+          	a = "add"
+          	ca = "commit --amend"
+          	cc = "commit"
+          	co = "checkout"
+          	f = "fetch"
+          	p = "push"
+          	st = "status"
+          	sw = "switch"
+
+          [core]
+          	editor = "nvim"
+          	quotePath = false
+
+          [merge]
+          	tool = "nvimdiff"
+
+          [diff]
+          	tool = "nvimdiff"
+
+          [includeIf "gitdir:~/work/"]
+          	path = "~/work/.gitconfig"
+        '';
       };
     };
-  };
 }
