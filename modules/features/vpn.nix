db625c2224c53262b475bfff5f547c733a3a4936
iva.nizelnik@gmail.com <sergtcorge@gmail.com>
2026-04-03 14:29:39 +0300

    downgrade vpn

--- modules/features/vpn.nix
+++ modules/features/vpn.nix
@@ -8,12 +8,13 @@
       enable = true;
       settingsFile = "/etc/v2ray/v2ray_config.json";
     };
+
     programs.throne = {
       enable = true;
-      package = pkgs.unstable.throne;
       tunMode.enable = true;
       tunMode.setuid = true;
     };
+
     networking.nameservers = ["1.1.1.1" "8.8.8.8" "8.8.4.4"];
   };
 }
