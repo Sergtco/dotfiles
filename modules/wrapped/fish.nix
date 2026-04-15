1b7eb7c91513742790c32e99f14acb609f673540
iva.nizelnik@gmail.com <sergtcorge@gmail.com>
2026-04-15 14:00:39 +0300

    simplify fish

--- modules/wrapped/fish.nix
+++ modules/wrapped/fish.nix
@@ -2,60 +2,48 @@
 {
   perSystem =
     { pkgs, lib, ... }:
-    let
-      plugins = with pkgs.fishPlugins; [
-        pure
-      ];
-
-      vendorDir = kind: plugin: "${plugin}/share/fish/vendor_${kind}.d";
-      complPath = map (vendorDir "completions") plugins;
-      funcPath = map (vendorDir "functions") plugins;
-      confPath = map (vendorDir "conf") plugins;
-
-      settings = pkgs.writeText "fish.conf" /* fish */ ''
-        set --prepend fish_complete_path ${lib.escapeShellArgs complPath}
-        set --prepend fish_function_path ${lib.escapeShellArgs funcPath}
-        set --local fish_conf_source_path ${lib.escapeShellArgs confPath}
-        for c in \$fish_conf_source_path/*; source \$c; end
-
-        alias v="nvim"
-        alias y="yazi"
-        alias d="docker"
-        alias dc="docker compose"
-        alias g="git"
-
-        fish_vi_key_bindings
-
-        fish_add_path ~/.cargo/bin ~/go/bin
-
-        ${lib.getExe pkgs.direnv} hook fish | source
-        ${lib.getExe pkgs.fzf} --fish | source
-        ${lib.getExe pkgs.zoxide} init fish | source
-
-        set --universal fish_transient_prompt 1
-
-        set --universal pure_show_numbered_git_indicator true
-        set --universal pure_enable_nixdevshell true
-        set --universal pure_symbol_nixdevshell_prefix '󱄅'
-      '';
-      extraPackages = with pkgs; [
-
-        direnv
-        fzf
-        zoxide
-      ];
-    in
     {
       packages.fish = inputs.wrapper-modules.lib.wrapPackage (
         { ... }:
         {
           inherit pkgs;
-          package = pkgs.fish;
-          extraPackages = extraPackages;
-          wrapperImplementation = "binary";
-          flags = {
-            "-C" = "source ${settings}";
+          package = pkgs.wrapFish {
+            runtimeInputs = with pkgs; [
+              direnv
+              fzf
+              zoxide
+            ];
+
+            pluginPkgs = with pkgs.fishPlugins; [
+              pure
+            ];
+
+            shellAliases = {
+              v = "nvim";
+              y = "yazi";
+              d = "docker";
+              dc = "docker compose";
+              g = "git";
+            };
+
+            localConfig = /* fish */ ''
+              fish_vi_key_bindings
+
+              fish_add_path ~/.cargo/bin ~/go/bin
+
+              ${lib.getExe pkgs.direnv} hook fish | source
+              ${lib.getExe pkgs.fzf} --fish | source
+              ${lib.getExe pkgs.zoxide} init fish | source
+
+              set --universal fish_transient_prompt 1
+
+              set --universal pure_show_numbered_git_indicator true
+              set --universal pure_enable_nixdevshell true
+              set --universal pure_symbol_nixdevshell_prefix '󱄅'
+            '';
           };
+
+          wrapperImplementation = "binary";
         }
       );
     };
